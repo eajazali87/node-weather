66bac5b8508e8d8db24b7a70f3957490c2b70eba
c4-team <EajazAli.MahaboobBasha@pearson.com>
2018-10-04 14:09:19 -0600

    return build status

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const request = require('request');
 const argv = require('yargs').argv;
 
-let apiKey = '5b0a4c0a2b7b19e69d3f4c05274a2a73';
-let city = argv.c;
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+let apiKey = 'e90179bdf7dfdfd05a88785ff7b1f36f';
+let url = `http://eajazali_mahaboobbasha:${apiKey}@10.201.255.161:8080/view/Functional-Tests/job/Autobahn_Admin_Services_Functional_Test/lastBuild/api/json`
+console.log(url)
 
 request(url, function(err, response, body) {
   if(err){
     console.log('error: ', err);
   } else {
     //console.log('body: ', body);
-    let weather = JSON.parse(body)
-    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    let buildResult = JSON.parse(body)
+    let message = `${buildResult.result}`;
     console.log(message);
   }
   });
